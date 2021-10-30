46b243ff5e358adf2fa5b6d48bb80fe03c75814f
obembium <peterjunior74@yahoo.com>
2021-10-30 23:09:44 +0100

    Delete src directory

--- src/Routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Home from './Page/Home'
-
-
-const route =  [
-{ path: "/" ,  Component: Home},
-  ]
-
-export default route;
--- src/Util/Constant.js
+++
@@ -1,9 +0,0 @@
-export const CONSTANT = {
-    domainApi:"http://api.football-data.org/v2/",
-    token:'token',
-    getCompetitions:'competitions',
-    getMatches:'matches',
-    getStandings:'standings',
-  
-    loading:'loading',
-}
